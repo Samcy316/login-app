77793a474bb32b6845df361c7280e9757836e334
Samson Kamau Mwangi <samsonkamau316@gmail.com>
2023-06-07 15:25:02 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import{MatIconModule}from'@angular/material/icon';
+import{ MatRadioModule} from'@angular/material/radio';
+import{ MatSelectModule} from'@angular/material/select';
+import{ MatDatepickerModule} from'@angular/material/datepicker';
+import { UserRegistrationService } from './user-registration.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  
   ],
-  providers: [],
+  providers: [UserRegistrationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
